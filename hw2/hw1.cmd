c39f79cdbd268696746e8ad4cd03579399dbe6d1
Mike Roylance <roylance.michael@gmail.com>
2015-01-14 18:40:26 -0700

    initial publish

---
+++ hw2/hw1.cmd
@@ -0,0 +1,16 @@
+####################
+#
+# Mike Roylance UW NetID roylance
+# LING 571 Homework 1
+#
+####################
+ 
+Universe   = vanilla
+ 
+Executable  = hw1.sh
+Arguments   = ""
+Log         = hw1.log
+Output      = hw1.out
+Error       = hw1.err
+Notification=Error
+Queue
---
+++ hw2/hw1.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+# Mike Roylance - roylance@uw.edu
+
+python2.6 source/main.py $1 $2
