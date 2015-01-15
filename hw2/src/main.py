ff0094f790ee75e1fe43c4dc7d20f86c3a6f920d
Mike Roylance <roylance.michael@gmail.com>
2015-01-14 18:42:34 -0700

    updated old code

--- hw2/src/main.py
+++ hw2/src/main.py
@@ -5,38 +5,7 @@ import re
 import sentenceTransform
 
 def main():
-    # build grammar file url
-    grammarFile = "file:" + sys.argv[1]
-    sentenceFile = sys.argv[2]
-
-    # read in grammar
-    grammar = nltk.data.load(grammarFile)
-    
-    # build chart parser
-    parser = nltk.parse.EarleyChartParser(grammar)
-
-    # create transformer
-    sentTransform = sentenceTransform.SentenceTransform(parser, nltk)
-
-    # read in sentences
-    input_stream = open(sentenceFile)
-
-    line = input_stream.readline()
-
-    # for each sentence
-    # delimit by space, print out bracked sentence
-    #print out number of parses
-# end, print out average
-
-    while(line):
-        dataTuple = sentTransform.parseSentence(line.strip().lower())
-
-print dataTuple[0]
-print dataTuple[1]
-
-line = input_stream.readline()
-
-print sentTransform.getAverageParses()
+    pass
 
 if __name__ == '__main__':
         main()
