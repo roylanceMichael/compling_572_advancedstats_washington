defad47d129a9bcc0e08dee1ad9e95b461e5a92a
toOliya <toOliya@gmail.com>
2015-01-26 14:19:47 +0700

    running on patas

---
+++ hw3/build_q5.cmd
@@ -0,0 +1,17 @@
+####################
+#
+# Mike Roylance UW NetID roylance
+# Olga Whelan UW Net ID olgaw
+# LING 572 Homework 3
+#
+####################
+ 
+Universe   = vanilla
+Executable  = produce_q5.sh
+getenv	    = true
+Arguments   = ""
+Log         = hw3.log
+Error       = hw3.err
+request_memory	= 2*1024
+Queue
+
