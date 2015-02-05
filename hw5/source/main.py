b98c969fab6689b6b016d2f6d1e0e91e15f2f333
toOliya <toOliya@gmail.com>
2015-02-05 14:38:19 +0700

    q1 working

--- hw5/source/main.py
+++ hw5/source/main.py
@@ -1,5 +1,7 @@
 import sys
+import operator
 import repo
+
 
 def main():
 	newRepo = repo.Repo()
@@ -7,9 +9,11 @@ def main():
 	# process this in some repository
 	for line in sys.stdin:
 		newRepo.read_into_dicts(line)
+	newRepo.getClassExpected()
+	newRepo.chiSq()
 
-	for key in newRepo.featureVector:
-		print "%s - %s" % (key, newRepo.featureVector[key])
+	for key in sorted(newRepo.chi2, key=newRepo.chi2.get, reverse=True):
+		print key, '\t', newRepo.chi2[key], '\t', newRepo.featureVector[key]
 
 if __name__ == '__main__':
     main()
--- hw5/source/repo.py
+++ hw5/source/repo.py
@@ -1,9 +1,16 @@
+from __future__ import division
 import re
+import math
 
 class Repo:
     def __init__(self):
         self.featureVector = {} # { featureName: count }
-        self.classVector = {}   # {className1: [{f1:1}, {f2:1}, {f3:1}], className2: []} - a list of feature vectors represented as dictionaries
+        self.classVector = {}   # {className1: {{f1:1}, {f2:1}, {f3:1}}, className2: {}} - a dictionary of feature vectors represented as dictionaries
+        self.classTotal = {}   # {className1: #feat-s, className2: #feat-s, ...}
+        self.classExpected = {}
+        self.chi2 = {}
+        self.total = 0
+        self.df = 0
 
 
     def read_into_dicts(self, line_of_input):
@@ -13,20 +20,58 @@ class Repo:
 
         if className not in self.classVector:
             self.classVector[className] = {}
-        
-        vectDict = {}
+
+        if className not in self.classTotal:
+            self.classTotal[className] = 0
+
         for i in ilist[1:]:
             # get features
             pair = i.split(':')
             if len(pair) == 2:
-                vectDict[pair[0]] = float(pair[1])
 
                 if pair[0] in self.featureVector:
                     self.featureVector[pair[0]] += 1
                 else:
                     self.featureVector[pair[0]] = 1
-
+             
                 if pair[0] in self.classVector[className]:
                     self.classVector[className][pair[0]] += 1
                 else:
                     self.classVector[className][pair[0]] = 1
+
+                self.classTotal[className] += 1
+
+
+    def getTotal(self):   
+        for key in self.classTotal:
+            self.total += self.classTotal[key]
+
+
+    def getClassExpected(self):
+        self.getTotal()
+        for key in self.classTotal:
+            for feat in self.featureVector:
+                self.classExpected[key] = self.classTotal[key] * self.featureVector[feat] / self.total
+
+
+    def chiSq(self):
+    # chiSq for every feature
+        for feat in self.featureVector:
+            chi = 0
+            
+            for className in self.classExpected:
+                if feat not in self.classVector[className]:
+                    self.classVector[className][feat] = 0
+                chi += math.pow((self.classVector[className][feat] - self.classExpected[className]), 2) / self.classExpected[className]
+
+            self.chi2[feat] = chi
+
+#    def chiSq(self):
+# calculates one number for the whole table               
+#        for feat in self.featureVector:
+#            for className in self.classExpected:
+#                self.chi2 += math.pow((self.classVector[className][feat] - self.classExpected[className]), 2) / self.classExpected[className]
+
+
+    def DF(self):
+        self.df = (len(self.featureVector) - 1) * (len(self.classTotal) - 1)
