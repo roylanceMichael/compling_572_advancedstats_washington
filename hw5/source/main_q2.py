9bac0614e09b9d2a7844cce3d4629a9662a7da8b
Mike Roylance <roylance.michael@gmail.com>
2015-02-09 21:17:48 -0700

    updated q4 results

--- hw5/source/main_q2.py
+++ hw5/source/main_q2.py
@@ -3,13 +3,17 @@ import re
 
 
 def main():
-	threshold = sys.argv[1]
+	sentP0Value = float(sys.argv[1])
 
-	# chiDictionary = { 0.10: 4.605, 0.05: 5.991, 0.025: 7.378, 0.01: 9.210, 0.001, 13.816}
+	chiDictionary = { 0:0, 0.10:4.605, 0.05:5.991, 0.025:7.378, 0.01:9.210, 0.001:13.816}
+
+	if sentP0Value not in chiDictionary:
+		print "please send a value that is in the chi dictionary!"
+		return
 	
 	for line in sys.stdin:
 		ilist = re.split('\s+', line)
-		if float(ilist[1]) >= float(threshold):
+		if float(ilist[1]) >= chiDictionary[sentP0Value]:
 			print line.strip()
 
 
