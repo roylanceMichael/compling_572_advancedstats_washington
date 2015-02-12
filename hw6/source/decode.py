e74cde931a278703682f6d7ddc57ea154d77e2d6
toOliya <toOliya@gmail.com>
2015-02-12 11:12:34 +0700

    output sys file

--- hw6/source/decode.py
+++ hw6/source/decode.py
@@ -3,19 +3,56 @@ import re
 import math
 
 class Decode:
-    def __init__(self):
-        self.doc = {}   # {className: {f1:None, f2:None, f3:None, ...}}
+    def __init__(self, m):
+        self.features = m.features
+        self.doc = {}   # {f1:None, f2:None, f3:None, ...}
+        self.docID = []
+        self.condProb = {}   # {className: prob, className: prob, ...} - not normalized yet
+        self.Z = 0
+        self.P = {}   # {className: prob, className: prob, ...} - final
 
 
-    def read_into_dicts(self, line_of_input):
+    def read_into_dicts(self, line_of_input, n):
         ilist = re.split('\s+', line_of_input.strip())
 
-        self.doc[ilist[0]] = {}
+        self.docID = [n, ilist[0]]
 
         for i in ilist[1:]:
             # get features
             pair = i.split(':')
             if len(pair) == 2:
-                self.doc[ilist[0]][pair[0]] = None
+                self.doc[pair[0]] = None
 
 
+    def getCondProb(self):
+        for className in self.features:
+            power = self.features[className]['<default>']
+            for f in self.doc:
+                if f in self.features[className]:
+                    power += self.features[className][f]
+
+            condProb = math.exp(power)
+#            print condProb
+            self.condProb[className] = condProb
+                    
+
+    def normaliZ(self):
+        for className in self.condProb:
+            self.Z += self.condProb[className]
+
+
+    def classCondProb(self):
+        self.normaliZ()
+        for className in self.condProb:
+#            print className
+#            print self.condProb[className]
+ #           print self.Z
+            self.P[className] = float(self.condProb[className] / self.Z)
+
+
+    def reportSysRecord(self):
+        stringBuilder = ""
+        for className in self.P:
+            stringBuilder += str(className) + " " + str(self.P[className]) + "\t "
+        
+        return "array:"+ str(self.docID[0]) + "\t" + str(self.docID[1]) + "\t" + stringBuilder 
--- hw6/source/main.py
+++ hw6/source/main.py
@@ -8,7 +8,7 @@ def main():
     # get from command parameters
     testData = sys.argv[1]
     modelFile = sys.argv[2]
-#    sysOutput = sys.argv[3]
+    sysOutput = sys.argv[3]
 
     m = model.Model()
     # read the model from file:
@@ -25,17 +25,34 @@ def main():
 	    mF_ = inF.readline()
 	    mF = mF_.strip()
 
-#    for key in m.features:
-#	    print key, len(m.features[key])
+    reportingDict = {}
+    for className in m.features:
+        reportingDict[className] = {}
+        for otherClassName in m.features:
+            reportingDict[className][otherClassName] = 0
+
+    correctTotal = 0
+    totalVectors = 0
+    currentIdx = 0
 
     # read in the training file
-    with open(testData) as inputF:
-        l = inputF.readline()
-        while len(l.strip()) > 0:
-            d = decode.Decode()
-            d.read_into_dicts(l)
+    with open(sysOutput, "w+") as outputF:
+        with open(testData) as inputF:
             l = inputF.readline()
-	    print d.doc
+            n = 0
+            while len(l.strip()) > 0:
+                d = decode.Decode(m)
+                d.read_into_dicts(l, n)
+            #            print d.doc
+                d.getCondProb()
+                d.classCondProb()
+            #            print d.P
+
+                # write to sys output
+                outputF.write(d.reportSysRecord() + "\n")
+                
+                l = inputF.readline()
+                n += 1
 
 
 if __name__ == '__main__':
