03b65fa11e822038238521df150db032354f85b6
toOliya <toOliya@gmail.com>
2015-02-14 13:07:14 +0700

    fixed q3

--- hw6/source/main_q3.py
+++ hw6/source/main_q3.py
@@ -9,13 +9,13 @@ def main():
 
     e = empiricalE.Empirical()
     # read the training data from file:
+    totalF = 0
     with open(trainingData) as inF:
         l = inF.readline()
         while len(l.strip()) > 0:
+            totalF += 1
             e.read_into_dicts(l)
             l = inF.readline()
-
-    totalF = len(e.allFeat)
 
     with open(outputFile, "w+") as outputF:
         outputF.write(e.outputEmpirical(totalF))
