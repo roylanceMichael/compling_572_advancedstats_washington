2fa9cf13e20823276c3828623be29b6876c88655
toOliya <toOliya@gmail.com>
2015-02-14 14:45:36 +0700

    q4

--- hw6/calc_model_exp.sh
+++ hw6/calc_model_exp.sh
@@ -3,6 +3,7 @@
 
 trainingData=$1
 outputFile=$2
+modelFile=$3
 
 if [ -z "$1" ]; then
 	trainingData="examples/train2.vectors.txt"
@@ -10,5 +11,8 @@ fi
 if [ -z "$2" ]; then
 	outputFile="output_file"
 fi
+if [ ! -z $3 ]; then 
+        modelFile="q1/m1.txt"
+fi
 
-python2.7 source/main_q4.py $trainingData $outputFile
+python2.7 source/main_q4.py $trainingData $outputFile ${modelFile}
