0824783c1a846e46c2228cdd3dd559b8826315aa
Mike Roylance <roylance.michael@gmail.com>
2015-02-17 20:49:24 -0700

    fixing maxent_classify output

--- hw6_java/maxent_classify.sh
+++ hw6_java/maxent_classify.sh
@@ -8,7 +8,6 @@ mkdir q2
 testData=$1
 modelFile=$2
 sysOutput=$3
-accOutput=$4
 
 if [ -z "$1" ]; then
 	testData="examples/test2.vectors.txt"
@@ -19,10 +18,7 @@ fi
 if [ -z "$3" ]; then
 	sysOutput="q2/res"
 fi
-if [ -z "$4" ]; then
-    accOutput="q2/acc"
-fi
 
 mvn clean -q
 mvn compile -q
-mvn exec:java -Dexec.mainClass="edu.washington.ling.roylance.MainQ2" -Dexec.args="${testData} ${modelFile} ${sysOutput}" -q > $accOutput
+mvn exec:java -Dexec.mainClass="edu.washington.ling.roylance.MainQ2" -Dexec.args="${testData} ${modelFile} ${sysOutput}" -q
